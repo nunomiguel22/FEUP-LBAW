35b0b89e0ab9d83cb13cf053a0dd5c52efd0d4bc
Nuno Miguel Marques <nunomiguel22@gmail.com>
2021-03-16 00:10:25 +0000

    cart update

--- html/cart.php
+++ html/cart.php
@@ -44,80 +44,83 @@
 
   <div class="container" style="padding-top:30px;">
 
-    <div class="row bg-dark b-shadow my-1">
-      <div class=" col-md-2"><img src="/img/carousel/CP2077.jpg" class="ml-0 mt-1"
-          style="max-width:96px;max-height:54px;">
+    <div class="row bg-transparent b-shadow my-1 px-2">
+
+      <div class="col-2 d-none d-lg-block">
+        <a href="/product_page_logged.php">
+          <img src="/img/carousel/CP2077.jpg" style="max-width:96px;max-height:54px;">
+        </a>
       </div>
-      <div class="col-5 m-auto">
+      <div class="col-4 m-auto">
         <div class="row">
           Cyberpunk 2077
         </div>
         <div class="row"><span class="HomeNav-devInfo">CD Projekt Red</span></div>
-
       </div>
 
 
-      <div class="col-md-2 my-auto" align="right">59.99€</div>
-      <button class="btn btn-secondary col-md-1" role="button">
-        <i class="far fa-trash-alt"></i>
-      </button>
+      <div class="col-6 my-auto ">
+        <div class="row ">
+          <span class="col text-right">59.99€</span>
+        </div>
+
+        <div class="row">
+          <a class="col text-right link-small text-light" href="#">Remove</a>
+        </div>
+      </div>
     </div>
 
-    <div class="row bg-dark b-shadow my-1">
-      <div class=" col-md-2"><img src="/img/carousel/H3.jpg" class="ml-0 mt-1" style="max-width:96px;max-height:54px;">
+    <div class="row bg-transparent b-shadow my-1 px-2">
+      <div class="col-2 d-none d-lg-block">
+        <a href="/product_page_logged.php">
+          <img src="/img/carousel/H3.jpg" style="max-width:96px;max-height:54px;">
+        </a>
       </div>
-      <div class="col-5 m-auto">
+      <div class="col-4 m-auto">
         <div class="row">
           Hitman 3
         </div>
-        <div class="row"><span class="HomeNav-devInfo">CD Projekt Red</span></div>
+        <div class="row"><span class="HomeNav-devInfo">IO Interactive</span></div>
 
       </div>
-
-
-      <div class="col-md-2 my-auto" align="right">30.00€</div>
-      <button class="btn btn-secondary col-md-1" role="button">
-        <i class="far fa-trash-alt"></i>
-      </button>
-    </div>
-
-    <div class="row bg-dark b-shadow my-1">
-      <div class=" col-md-2"></div>
-      <div class="col-5 m-auto">
-        <div class="row my-3">
-          <h5>Total</h5>
+      <div class="col-6 my-auto ">
+        <div class="row ">
+          <span class="col text-right">30.00€</span>
         </div>
 
-
+        <div class="row">
+          <a class="col text-right link-small text-light" href="#">Remove</a>
+        </div>
       </div>
-
-
-      <div class="col-md-2 my-auto" align="right">
-        <h5>89.99€</h5>
-      </div>
-      <button class="btn btn-secondary col-md-1" role="button">
-        <i class="far fa-trash-alt"></i>
-      </button>
     </div>
 
+    <div class="row bg-dark text-light mt-3 p-2">
 
+      <div class="col col-sm-4 my-3">
+        Estimated Total*
+      </div>
 
-
-
-    <hr style="background:white;">
-
-
-    <div class="row">
-
-      <button class="btn btn-lg col-2 col-sm-4 btn-danger mr-3 mb-3" href="/index.php" role="button">
-        <i class="fas fa-trash"></i> Remove all
-      </button>
-
-
-      <button class="btn btn-lg btn-success col-2 col-sm-4 mb-3" href="/index.php" role="button">
-        <i class="fas fa-shopping-cart"></i> Checkout
-
+      <div class="col">
+        <div class="row">
+          <span class="col my-3 text-right font-weight-bold">89.99€</span>
+        </div>
+      </div>
     </div>
+
+    <div class="row bg-dark text-light p-2" align="right">
+      <div class="col">
+        <button class="btn btn-danger mb-2" href="/index.php" role="button">
+          <i class="fas fa-trash"></i> Remove all
+        </button>
+
+        <button class="btn btn-success mb-2" href="/index.php" role="button">
+          <i class="fas fa-shopping-cart"></i> Checkout
+        </button>
+      </div>
+    </div>
+
+    <span class="row text-light ml-1 my-3 small">*All prices include VAT where applicable</span>
+
 
   </div>
 
