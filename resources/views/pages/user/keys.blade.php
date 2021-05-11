7049633e497cc536614d16a9dfe68ae7385f1622
Nuno Miguel Marques <nunomiguel22@gmail.com>
2021-05-11 22:56:58 +0100

    Suggested changes to My Keys

--- resources/views/pages/user/keys.blade.php
+++ resources/views/pages/user/keys.blade.php
@@ -1,19 +1,19 @@
 <div class="container mt-4 px-0">
     <h4 class="text-shadow">MY KEYS</h4>
     <div class="row">
-        <div class="col-7">
+        <div class="col-8">
             <span class="text-muted">Browser your purchase history and game keys</span>
         </div>
-        <div class="col-3">
+        <div class="col-4">
             <span class="text-muted">Filter status</span>
         </div>
     </div>
     <div class="row">
-        <div class="col-7">
+        <div class="col-8">
             <input type="text" class="form-control bg-secondary text-light w-100 mt-2" placeholder="Search for a game"
                 id="key_search_field">
         </div>
-        <div class="col-3">
+        <div class="col-4">
             <select id="key_filter" class="form-control mt-2 bg-secondary text-light">
                 <option selected="selected">All</option>
                 <option value="Completed">Completed</option>
@@ -21,7 +21,6 @@
                 <option value="Aborted">Aborted</option>
             </select>
         </div>
-        <div class="col-2 my-auto" id="list-loader"></div>
     </div>
 </div>
 
@@ -33,7 +32,7 @@
         <div class="col-2 my-auto">Price</div>
         <div class="col-2 my-auto d-none d-md-block">Date</div>
         <div class="col my-auto d-none d-md-block"> Payment Method</div>
-        <div class="col-2 my-auto mr-1 d-none d-md-block">Status</div>
+        <div class="col-2 my-auto mr-1">Status</div>
         <div class="col my-auto">Key</div>
 
     </article>
@@ -44,6 +43,8 @@
 
 
 </section>
+
+<aside id="spinner_loader" class="row my-3"></aside>
 
 <aside id="list-links" class="container">
 
@@ -72,9 +73,21 @@
                     </div>
                 </div>
 
-                <div class="row my-4">
+                <div class="row mt-4">
                     <h4 id="modal_title" class="col text-light"></h4>
                 </div>
+
+                <div class="row my-2">
+                    <span class="col">Product Key</span>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <input id="modal_key" type="text" class="form-control text-light bg-secondary" readonly>
+                    </div>
+                </div>
+
+
                 <hr>
                 <div class="row my-4">
                     <div class="col-4">
@@ -98,11 +111,8 @@
                     <div class="col-4 my-auto">
                         Price: <span id="modal_price" class="text-light"></span> EUR
                     </div>
-                    <div class="col">
-                        <input id="modal_key" type="text" class="form-control text-light bg-secondary" readonly>
-                    </div>
-                </div>
 
+                </div>
             </div>
         </div>
     </div>
