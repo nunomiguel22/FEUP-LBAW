9e6bb7a2db85c5df04071cc732f0e49a0594e667
Nuno Miguel Marques <nunomiguel22@gmail.com>
2021-05-18 14:43:07 +0100

    ui update

--- resources/views/pages/product_page.blade.php
+++ resources/views/pages/product_page.blade.php
@@ -23,10 +23,10 @@
                 <article class="carousel-inner">
                     <div class="carousel-item active">
                         <img src="{{ $game->cover_image() }}" class="d-block w-100" alt="game_img">
-                        @php
-                        $isFirst = true
-                        @endphp
                     </div>
+                    @php
+                    $isFirst = true
+                    @endphp
                     @forelse ($game->images as $image)
                     @php
                     if($isFirst) {
@@ -50,16 +50,12 @@
                 </article>
             </div>
 
-
-
             <div class="row mt-2">
                 @if(Auth::check())
-
                 <button type="button" class="btn btn-success mb-2 mr-2"><i class="fas fa-shopping-cart"></i> Add to
                     cart</button>
                 <button type="button" class="btn btn-secondary mb-2 mr-2"><i class="far fa-heart"></i>Add to
                     wishlist</button>
-
                 @else
                 <button type="button" class="btn btn-success mb-2 mr-2" data-toggle="modal" data-target="#LoginModal">
                     <i class="fas fa-shopping-cart"></i> Add to
@@ -68,27 +64,27 @@
                 <button type="button" class="btn btn-secondary mb-2 mr-2" data-toggle="modal" data-target="#LoginModal">
                     <i class="far fa-heart"></i>Add to
                     wishlist</button>
-
                 @endif
             </div>
 
         </div>
 
         <div class="col">
+            <h3 class="row">{{$game->title ?? null}}</h3>
             <div class="row">
                 <div class="col">
-                    <h3 class="row">{{$game->title ?? null}}</h3>
+
+
+                    <h6 class="row text-light"> Developer </h6>
                     <p class="row text-muted">{{$game->developers->name ?? null}}</p>
                 </div>
-                <div class="col mt-2">
-                    <div class="row">
-                        <h5>Released on &nbsp;</h5>
-                        <span class="text-muted">{{$game->launch_date ?? null}}</span>
-                    </div>
-                    <h4 class="row">{{$game->price ?? null}} €</h4>
+                <div class="col">
+
+                    <h6 class="row text-light"> Released on </h6>
+                    <span class="row text-muted">{{$game->launch_date ?? null}}</span>
                 </div>
             </div>
-
+            <h6 class="row text-light"> About {{$game->title}}</h6>
             <div class="row">
                 <p style="font-size:100%;">
                     {{$game->description ?? null}}
@@ -96,29 +92,31 @@
             </div>
 
             <div class="row">
-                <div class="col">
-                    <div class="row mt-3">
-                        <h4> Tags </h4>
-                    </div>
-                    <div class="row">
-                        <span class="text-muted" style="word-spacing: 2px;">
-                            @forelse ($game->tags as $tag)
-                            {{$tag->name}}
-                            @empty
-                            No tags yet!
-                            @endforelse
-                        </span>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="radialProgressBar-large progress-{{$percent}}">
-                        <div class="overlay text-light">{{$game->score ?? null}}</div>
-                    </div>
+
+                <div class="col-4 col-md-2">
+                    <h6 class="row text-light"> Price </h6>
+                    <h5 class="row">{{$game->price ?? null}} €</h5>
                 </div>
 
+                <div class="col">
+                    <h6 class="row text-light"> Tags </h6>
+                    <span class="text-muted row" style="word-spacing: 2px;">
+                        @forelse ($game->tags as $tag)
+                        {{$tag->name}}
+                        @empty
+                        No tags yet!
+                        @endforelse
+                    </span>
+                </div>
+
+                <div class="col-3 m-0 p-0 ">
+                    <div class="radialProgressBar progress-{{$percent}} m-0 p-0">
+                        <div class="overlay text-light ">{{$game->score ?? null}}</div>
+                    </div>
+                </div>
             </div>
-        </div>
 
+        </div>
     </div>
 
 </div>
